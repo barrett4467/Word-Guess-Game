664a11f44044de9640b8d267c97a1c97bf02022e
Rebecca Barrett <barrett4467@gmail.com>
2019-09-05 17:12:48 -0600

    Stores letter guessed in empty array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var availableWords = ["ankle"];
-var currentWord = ["_" , "_" , "_","_", "_"]
+var currentWord = [];
 var attemptsRemaining = 10;
 var wins = 0;
 
@@ -18,13 +18,116 @@ var winsText = document.getElementById("wins");
 document.onkeyup = function(event) {
     var wordChosen = availableWords[Math.floor(Math.random() * availableWords.length)]; //gives random word
     var letterGuessed = event.key; //key user presses
+    var pastGuesses = [];
 
-    var letter = availableWords[0].charAt[0]  ///Gives us a!! 
-        if (wordChosen[0] === letterGuessed){ 
-            currentWordText.textContent = (letterGuessed + "_ _ _ _");
-        
+    // var letter = availableWords[0].charAt[0]  ///Gives us first letter!! 
+
+    var letter = wordChosen[0];
+
+    if (wordChosen[0] === letterGuessed) { 
+        currentWordText.textContent = (letterGuessed);
+        console.log(letterGuessed);
+
+        pastGuesses.push(letterGuessed);
+        console.log(pastGuesses);
+
+    } else if ((wordChosen[0] != letterGuessed) && (wordChosen[1] === letterGuessed)){
+                currentWordText.textContent = (pastGuesses[1] + letterGuessed);
+
+                pastGuesses.push(letterGuessed);
+                console.log(pastGuesses)
+            }
     }
-}
+
+        //    else if ((wordChosen[0] === letterGuessed) && (wordChosen[1] === letterGuessed)){
+        //        currentWordText.textContent = (letterGuessed + "_ _ _");
+        //     }
+    // }
+
+
+    // switch(keyPressed){ 
+    //     case "a": 
+            
+    //         break;
+    //     case "b":
+            
+    //         break;
+    //     case "c":
+            
+    //         break;
+    //     case "d":
+            
+    //         break;
+    //     case "e": 
+            
+    //         break;
+    //     case "f":
+            
+    //         break;
+    //     case "g":
+            
+    //         break;
+    //     case "h":
+            
+    //         break;
+    //     case "i": 
+            
+    //         break;
+    //     case "j":
+            
+    //         break;
+    //     case "k":
+            
+    //         break;
+    //     case "l":
+
+    //         break;
+    //     case "m": 
+        
+    //         break;
+    //     case "n":
+            
+    //         break;
+    //     case "o":
+            
+    //         break;
+    //     case "p":
+            
+    //         break;
+    //     case "q": 
+            
+    //         break;
+    //     case "r":
+            
+    //         break;
+    //     case "s":
+            
+    //         break;
+    //     case "t":
+            
+    //         break;
+    //     case "u": 
+        
+    //         break;
+    //     case "v":
+            
+    //         break;
+    //     case "w":
+            
+    //         break;
+    //     case "x":
+            
+    //         break;
+    //     case "y": 
+            
+    //         break;
+    //     case "z":
+            
+    //         break;
+    //     default:
+    //        return alert("Invalid Key try again!");
+
+    // }
 
 
 
@@ -42,12 +145,6 @@ document.onkeyup = function(event) {
     //         }
     //      }   
     //     }
-
-                // (letterGuessed === "n") ||
-                // (letterGuessed === "k") ||
-                // (letterGuessed === "l") ||
-                // (letterGuessed === "e")) {
-             //wrong but will fix later
                 
 
         // } else {
@@ -60,6 +157,3 @@ document.onkeyup = function(event) {
         // letterGuessedText.textContent = "Letters guessed: " + letterGuessed;
         // attemptsRemainingText.textContent = "Attempts remaining " + attemptsRemaining;
         // winsText.textContent = "Wins " + wins;
-
-
-        //I don't know how much of this is a fever dream...
